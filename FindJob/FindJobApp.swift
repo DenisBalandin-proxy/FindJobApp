b19428480f0494052586e363ff73384bec75ebc9
Denis Balandin <doublestar2013@yandex.ru>
2024-03-19 09:40:00 +0300

    Initial Commit

---
+++ FindJob/FindJobApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindJobApp.swift
+//  FindJob
+//
+//  Created by Denis on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FindJobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
